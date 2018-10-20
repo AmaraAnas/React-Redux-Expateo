a86f845346bec99412c9ff4abf2bc09dc0d34503
zazapeta <sami.ghazouane@gmail.com>
2018-10-21 00:18:47 +0200

    Added LogRocket to track UX impact

--- client/src/redux-utils/store.js
+++ client/src/redux-utils/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { isFSA } from 'flux-standard-action';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 
 import reducers from './reducers';
 
 const middlewares = [thunk];
 
+// ---------------------------------------------------------------------
 let composeEnhancers = compose;
-
 if (process.env.NODE_ENV === 'development') {
   const fsaChecker = (store) => (next) => (action) => {
     if (isFSA(action)) {
@@ -25,7 +27,20 @@ if (process.env.NODE_ENV === 'development') {
   composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancers;
 }
+// ---------------------------------------------------------------------
+if (process.env.NODE_ENV === 'production') {
+  LogRocket.init('1snrjg/develop-expateo-pa');
+  //   LogRocket.identify('0123456789', {
+  //     name: 'James Morrison',
+  //     email: 'jamesmorrison@example.com',
 
+  //     // Add your own custom user variables here, ie:
+  //     subscriptionType: 'pro',
+  //   });
+  setupLogRocketReact(LogRocket);
+  middlewares.push(LogRocket.reduxMiddleware());
+}
+// ---------------------------------------------------------------------
 const store = createStore(
   reducers,
   composeEnhancers(applyMiddleware(...middlewares)),
