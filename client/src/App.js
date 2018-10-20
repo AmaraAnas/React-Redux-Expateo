a2aaf5d06ab1bac7005428e38fb94abc6157524e
zazapeta <sami.ghazouane@gmail.com>
2018-10-20 15:53:48 +0200

    testing CI/CD with trello

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Wow,the CI/CD workflow is good =)
           </a>
         </header>
       </div>
