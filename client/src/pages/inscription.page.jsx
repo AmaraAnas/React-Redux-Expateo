046d55b4def018ee397ad453c00a53957ce9835c
zazapeta <sami.ghazouane@gmail.com>
2018-12-08 02:33:24 +0100

    Simplfied and modularized the inscription page'sstyle

--- client/src/pages/inscription.page.jsx
+++ client/src/pages/inscription.page.jsx
@@ -1,44 +1,36 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import '../styles/inscription.page.css';
+import { Container } from 'semantic-ui-react';
+
+import styles from './inscription.page.module.css';
 import InscriptForm from '../pills/inscription/inscription.container';
 
+//TODO: handle the submit form
 export default class InscriptionForm extends React.Component {
   constructor(props) {
     super(props);
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
   }
+
   handleLoginSubmit(user) {
     this.setState();
   }
+
   render() {
     return (
-      <Grid
-        textAlign="center"
-        style={{
-          height: '100%',
-          background: 'rgba(255,255,255,255)',
-        }}
-      >
-        <Grid.Column
-          style={{
-            maxWidth: '90%',
-            width: '600px',
-          }}
-        >
-          <h1 className="bienvenulabel">
-            Bienvenu sur votre espace personnel Expateo
-          </h1>
-          <p className="bienvenuparagraph">
-            Grâce à votre entreprise vous allez bénéficier d’un accompagnement
-            pour vous aider étape par étape dans votre mobilité.
-            <br />
-            Pour y accéder créez votre mot de passe et répondez aux questions
-            nécessaires au bon fonctionnement de l’application
-          </p>
-          <InscriptForm onLogin={this.handleLoginSubmit} />
-        </Grid.Column>
-      </Grid>
+      <Container className={styles.container} text fluid>
+        <h1 className={styles.title}>
+          Bienvenu sur votre espace personnel Expateo
+        </h1>
+        <p className={styles.text}>
+          Grâce à votre entreprise vous allez bénéficier d’un accompagnement
+          pour vous aider étape par étape dans votre mobilité.
+        </p>
+        <p className={styles.text}>
+          Pour y accéder créez votre mot de passe et répondez aux questions
+          nécessaires au bon fonctionnement de l’application
+        </p>
+        <InscriptForm onLogin={this.handleLoginSubmit} />
+      </Container>
     );
   }
 }
