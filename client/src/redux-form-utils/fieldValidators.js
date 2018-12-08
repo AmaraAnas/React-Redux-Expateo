b82f106dd0d0304e2339544836fb44363562b5f7
zazapeta <sami.ghazouane@gmail.com>
2018-12-08 02:30:11 +0100

    Moved validaor into fieldValidatorsFile (TODO added)

--- client/src/redux-form-utils/fieldValidators.js
+++ client/src/redux-form-utils/fieldValidators.js
@@ -1,3 +1,5 @@
+export const optional = () => undefined;
+
 export const required = (value) => (value ? undefined : 'required');
 
 export const regexTest = (regex, error) => (value) =>
@@ -46,3 +48,41 @@ export const withMinLower = (len) =>
     new RegExp(`(.*[a-z]){${len},}.*`),
     `Must contain at least ${len} lowercase char`,
   );
+
+// TODO: Remove or test them. WARN about duplicata
+// const hasNumber = (value) => {
+//   return new RegExp(/[0-9]/).test(value);
+// };
+
+// const hasMixed = (value) => {
+//   return new RegExp(/[a-z]/).test(value) && new RegExp(/[A-Z]/).test(value);
+// };
+
+// const hasSpecial = (value) => {
+//   return new RegExp(/[!#@$%^&*)(+=._-]/).test(value);
+// };
+
+// export const strengthColor = (count) => {
+//   if (count == -1) return '';
+
+//   if (count < 2) return 'red';
+
+//   if (count < 4) return 'orange';
+
+//   if (count < 6) return 'green';
+// };
+
+// export const strengthIndicator = (value) => {
+//   let strengths = 0;
+//   if (value.length == 0) strengths = -1;
+
+//   if (value.length > 12) strengths++;
+
+//   if (hasNumber(value)) strengths++;
+
+//   if (hasSpecial(value)) strengths++;
+
+//   if (hasMixed(value)) strengths++;
+
+//   return strengths;
+// };
