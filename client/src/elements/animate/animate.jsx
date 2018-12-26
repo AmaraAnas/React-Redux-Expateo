466d6e11e3172bcee7ba7c42901b3aaaeb980a14
zazapeta <sami.ghazouane@gmail.com>
2018-12-26 13:48:38 +0100

    Added Animate to handle simple animation

---
+++ client/src/elements/animate/animate.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'animate.css/animate.min.css';
+
+/**
+ * Simple wrapper on http://daneden.github.io/animate.css
+ */
+export default function Animate({ animation, children }) {
+  return React.cloneElement(children, { className: `animated ${animation}` });
+}
+
+Animate.propTypes = {
+  /**
+   * @see https://github.com/daneden/animate.css#animations
+   */
+  animation: PropTypes.oneOf(['fadeOutRight', 'fadeInRight', 'bounce'])
+    .isRequired,
+};
