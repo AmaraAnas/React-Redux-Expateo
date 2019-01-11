0cfcc9ad720b5c7df953ab8838c7b6495b174a4e
zazapeta <sami.ghazouane@gmail.com>
2019-01-11 15:54:55 +0100

    [WIP] Menu bar mobile version

--- client/src/pills/nav/nav.mobile.view.jsx
+++ client/src/pills/nav/nav.mobile.view.jsx
@@ -1,36 +1,89 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
 
-import { Menu, Dropdown, Image } from '../../ui-kit';
-import logo from '../../images/logo-sans-fond_nopadding.png';
+import t from '../../i18n';
+import {
+  Button,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+} from '../../ui-kit';
+import logo from '../../logo.svg';
 
-const NavMobileView = ({ children }) => (
-  <>
-    {children}
-    <Menu attached="top" size="massive">
-      <Menu.Menu position="left">
-        <Image src={logo} style={{ minWidth: '100%', height: '100px' }} />
-      </Menu.Menu>
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <h1>Messagerie</h1>
-        </Menu.Item>
-        <Menu.Item>
-          <h1>Notifications</h1>
-        </Menu.Item>
-        <Menu.Item>
-          <h1>Documents</h1>
-        </Menu.Item>
-        <Dropdown item text="Profil">
-          <Dropdown.Menu>
-            <Dropdown.Item>English</Dropdown.Item>
-            <Dropdown.Item>Russian</Dropdown.Item>
-            <Dropdown.Item>Spanish</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Menu.Menu>
-    </Menu>
-  </>
-);
+import SubNavDesktopView from './subNav.desktop.view';
+
+class NavMobileView extends React.Component {
+  state = { visible: false };
+
+  handleHideClick = () => this.setState({ visible: false });
+  handleShowClick = () => this.setState({ visible: true });
+  handleSidebarHide = () => this.setState({ visible: false });
+
+  render() {
+    const { visible } = this.state;
+    const { children } = this.props;
+
+    return (
+      <>
+        <Button.Group
+          style={{ position: 'absolute', zIndex: 9999, top: 0, left: '100px' }}
+        >
+          <Button disabled={visible} onClick={this.handleShowClick}>
+            Show sidebar
+          </Button>
+        </Button.Group>
+
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="push"
+            onHide={this.handleSidebarHide}
+            visible={visible}
+            width="wide"
+            icon="labeled"
+            vertical
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              border: '0',
+            }}
+          >
+            <div>
+              <Icon
+                name="close"
+                disabled={!visible}
+                onClick={this.handleHideClick}
+                size="large"
+                style={{
+                  top: '5px',
+                  right: '5px',
+                  position: 'relative',
+                  float: 'right',
+                }}
+              />
+              <Image src={logo} floated="left" size="tiny" />
+            </div>
+            <Menu.Item>
+              <Icon name="home" />
+              Home
+            </Menu.Item>
+            <Menu.Item>
+              <Icon name="gamepad" />
+              Games
+            </Menu.Item>
+            <Menu.Item>
+              <Icon name="camera" />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={visible}>{children}</Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </>
+    );
+  }
+}
 
 export default NavMobileView;
