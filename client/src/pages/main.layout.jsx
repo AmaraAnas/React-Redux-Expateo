4737a3ad48fd96073f8598ab48d99d9dad08b871
zazapeta <sami.ghazouane@gmail.com>
2019-01-12 23:39:48 +0100

    Fixed layout to be full height adaptative with avalaible screen, not with the screen sice that lie to us on what the user are  seeing

--- client/src/pages/main.layout.jsx
+++ client/src/pages/main.layout.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 
 import Nav from '../pills/nav/nav.container';
-import { Container } from '../ui-kit';
 
 const MainLayout = ({ children, isNavVisible }) => {
   return (
-    <div
-      style={{
-        backgroundColor: 'white',
-        height: '100vh',
-        //   overflowX: 'hidden',
-        //   overflowY: 'auto',
-      }}
-    >
+    <div className="main-layout">
       {(isNavVisible && <Nav>{children}</Nav>) || children}
     </div>
   );
