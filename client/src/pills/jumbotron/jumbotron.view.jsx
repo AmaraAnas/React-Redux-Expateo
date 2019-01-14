d9fe0d4a3375d8da93967c4b239f539628204c5d
AmaraAnas <anas94amara@gmail.com>
2019-01-14 18:13:46 +0100

    refacto Segment content

--- client/src/pills/jumbotron/jumbotron.view.jsx
+++ client/src/pills/jumbotron/jumbotron.view.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Button, Header, Icon, Segment } from '../../ui-kit';
+import { Button, Header, Icon, Segment, Grid, Image } from '../../ui-kit';
 
 const SegmentJumbotronView = () => (
-  <Segment inverted attached>
-    <Header icon>
-      <Icon name="search" />
-      We don't have any documents matching your query.
-    </Header>
-    <Segment.Inline>
-      <Button primary>Clear Query</Button>
-      <Button>Add Document</Button>
-    </Segment.Inline>
+  <Segment padded="very" attached secondary>
+    <Grid centered columns={2} divided>
+      <Grid.Row>
+        <Grid.Column textAlign="right" verticalAlign="middle">
+          J -85
+        </Grid.Column>
+        <Grid.Column textAlign="left" verticalAlign="middle">
+          Votre départ pour Tunisie <br />
+          prévu le mardi 25 Mars 2021
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Button content="Personaliser ma checklist" icon="signup" size="big" />
+      </Grid.Row>
+    </Grid>
   </Segment>
 );
 
