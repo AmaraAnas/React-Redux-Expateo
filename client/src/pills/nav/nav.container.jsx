f51e637c4ad92c307bb8827eb172c31b2d142291
zazapeta <sami.ghazouane@gmail.com>
2019-01-16 01:11:57 +0100

    clean imports order

--- client/src/pills/nav/nav.container.jsx
+++ client/src/pills/nav/nav.container.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { useDispatch, withHook } from '../../hooks/useDispatch.hook';
 import Theme from '../../models/theme.model';
+
 import { themesByTasksSelector } from '../themes/themes.selectors';
 import { getThemes } from '../themes/themes.actions';
 import { getTasks } from '../tasks/tasks.actions';
 
 import NavResponsive from './nav.responsive.view';
-import { useDispatch, withHook } from '../../hooks/useDispatch.hook';
 
 const NavContainer = ({ themes, children }) => {
   return <NavResponsive themes={themes}>{children}</NavResponsive>;
