2faf240ea9cc5e6f974b99ea2486cd1351fdf596
zazapeta <sami.ghazouane@gmail.com>
2019-01-17 20:09:53 +0100

    Integrated mobility data into the jumbotron

--- client/src/pills/jumbotron/jumbotron.view.jsx
+++ client/src/pills/jumbotron/jumbotron.view.jsx
@@ -1,50 +1,53 @@
 import React from 'react';
-import { Button, Header, Icon, Segment, Grid, Label } from '../../ui-kit';
-import background from '../../images/jumborton-background.png';
+import PropTypes from 'prop-types';
+
+import { Icon, Segment, Grid } from '../../ui-kit';
+import Animate from '../../elements/animate/animate';
+import Mobility from '../../models/mobility.model';
+
 import styles from './jumbotron.module.css';
-import t from '../../i18n';
 
-let userInfos = {
-  mobilityTitle: 'Titre de la mobilité',
-  destination: 'Tunisie',
-  date: '30/02/2019',
-};
-
-export default function SegmentJumbotronView({ mobilitiyInfo }) {
+export default function SegmentJumbotronView({ mobility }) {
   return (
     <Segment size="large" attached placeholder>
-      <Grid columns={2}>
-        {/*
-        <Grid.Row>
-          <Label as="a" size="big" style={{ background: "transparent"}}>
-            <Icon name='angle left' /> {t('jumborton.all_mobs')}
-          </Label> 
-        </Grid.Row> 
-      */}
+      {mobility ? (
+        <Animate animation="fadeInDown">
+          <Grid columns={2}>
+            {/* <Grid.Row>
+              <Label as="a" size="big" style={{ background: 'transparent' }}>
+                <Icon name="angle left" /> {t('jumborton.all_mobs')}
+              </Label>
+            </Grid.Row> */}
 
-        <Grid.Row centered className={styles.title}>
-          {userInfos.mobilityTitle}
-        </Grid.Row>
-
-        <Grid.Row centered>
-          <Grid.Column
-            textAlign="right"
-            verticalAlign="middle"
-            className={styles.text}
-          >
-            <Icon name="calendar alternate outline" />
-            {userInfos.date}
-          </Grid.Column>
-          <Grid.Column
-            textAlign="left"
-            className={styles.text}
-            verticalAlign="middle"
-          >
-            <Icon name="map marker alternate" />
-            {userInfos.destination}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+            <Grid.Row centered className={styles.title}>
+              {mobility.destination}
+            </Grid.Row>
+            <Grid.Row centered>
+              <Grid.Column
+                textAlign="right"
+                verticalAlign="middle"
+                className={styles.text}
+              >
+                <Icon name="calendar alternate outline" />
+                {mobility.startDate &&
+                  new Date(mobility.startDate).toLocaleDateString()}
+              </Grid.Column>
+              <Grid.Column
+                textAlign="left"
+                className={styles.text}
+                verticalAlign="middle"
+              >
+                <Icon name="map marker alternate" />
+                {mobility.destination}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Animate>
+      ) : null}
     </Segment>
   );
 }
+
+SegmentJumbotronView.propTypes = {
+  mobility: PropTypes.instanceOf(Mobility),
+};
