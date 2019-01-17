7bb3b95f3fda648112a1abf7bc73be330e84d577
AmaraAnas <anas94amara@gmail.com>
2019-01-17 11:43:56 +0100

    Mobilty API refacto + Ensure that we have correct data by testing properties

--- client/src/pills/mobility/mobility.api.js
+++ client/src/pills/mobility/mobility.api.js
@@ -2,7 +2,11 @@ import Mobility from '../../models/mobility.model';
 
 import { mobilityApi } from '../api/base.api';
 
-//TODO : Refacto to remove magic strings
+const mobQuestionsIDs = {
+  mob_destination_QUE_GUID: 'QUE_88',
+  mob_date_QUE_GUID: 'QUE_20',
+};
+
 export async function getMobility({ sessionId: gSesGuid, id: gUsrId }) {
   return await mobilityApi({
     gSesGuid,
@@ -12,14 +16,26 @@ export async function getMobility({ sessionId: gSesGuid, id: gUsrId }) {
   }).then((rawQuestion) => {
     let mobility = {
       id: 1,
-      startDate: '',
-      destination: '',
+      startDate: null,
+      destination: null,
     };
     rawQuestion.map((question) => {
-      if (question.QUE_GUID == 'QUE_20') {
-        mobility.startDate = question.user_answer_done[0].USA_DATE;
-      } else if (question.QUE_GUID == 'QUE_88') {
-        mobility.destination = question.user_answer_done[0].USA_TEXT;
+      if (question.QUE_GUID == mobQuestionsIDs.mob_date_QUE_GUID) {
+        if (
+          question.hasOwnProperty('user_answer_done') &&
+          question.user_answer_done[0] != null
+        ) {
+          mobility.startDate = question.user_answer_done[0].USA_DATE;
+        }
+      } else if (
+        question.QUE_GUID == mobQuestionsIDs.mob_destination_QUE_GUID
+      ) {
+        if (
+          question.hasOwnProperty('user_answer_done') &&
+          question.user_answer_done[0] != null
+        ) {
+          mobility.destination = question.user_answer_done[0].USA_TEXT;
+        }
       }
     });
     return new Mobility(mobility);
