6b471ab61772e3c3cdbc740126047a193ceb0872
zazapeta <sami.ghazouane@gmail.com>
2019-01-18 12:56:20 +0100

    Ugly fix to avoid collapsing menus on fitted screens

--- client/src/pills/nav/desktop/nav.desktop.subNav.view.jsx
+++ client/src/pills/nav/desktop/nav.desktop.subNav.view.jsx
@@ -9,53 +9,77 @@ import Service from '../../../models/service.model';
 
 // TODO: simple tests
 
-const SubNavDesktopView = ({ themes, services, collapsed }) => (
-  <Menu fluid widths={3} attached={!collapsed}>
-    <Dropdown item text={t('menu.subNav.themes')}>
-      <Dropdown.Menu>
-        {themes.map((theme, i) => (
-          <Dropdown.Item
-            key={i}
-            text={theme.label}
-            as={Link}
-            to={`/themes/${theme.id}`}
-          />
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
-    <Dropdown item text={t('menu.subNav.services')}>
-      <Dropdown.Menu>
-        <Dropdown.Header>{t('menu.subNav.obligation')}</Dropdown.Header>
-        {services
-          .filter((service) => service.isRequired)
-          .map((service, i) => (
+const CollapsedSubNav = ({ children }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const SubNav = ({ children }) => {
+  return (
+    <Menu fluid widths={3} attached>
+      {children}
+    </Menu>
+  );
+};
+
+const SubNavDesktopView = ({ themes, services, collapsed }) => {
+  const Nav = collapsed ? CollapsedSubNav : SubNav;
+  return (
+    <Nav>
+      <Dropdown item text={t('menu.subNav.themes')}>
+        <Dropdown.Menu>
+          {themes.map((theme, i) => (
             <Dropdown.Item
               key={i}
-              description="obligatoire"
-              text={service.title}
+              text={theme.label}
               as={Link}
-              to={`/services/${service.id}`}
+              to={`/themes/${theme.id}`}
             />
           ))}
-        <Divider />
-        <Dropdown.Header>{t('menu.subNav.optionnel')}</Dropdown.Header>
-        {services
-          .filter((service) => !service.isRequired)
-          .map((service, i) => (
-            <Dropdown.Item
-              key={i}
-              text={service.title}
-              as={Link}
-              to={`/services/${service.id}`}
-            />
-          ))}
-      </Dropdown.Menu>
-    </Dropdown>
-    <Menu.Item as={Link} to="/bills" className="text">
-      {t('menu.subNav.factures')}
-    </Menu.Item>
-  </Menu>
-);
+        </Dropdown.Menu>
+      </Dropdown>
+      <Dropdown item text={t('menu.subNav.services')}>
+        <Dropdown.Menu>
+          <Dropdown.Header>{t('menu.subNav.obligation')}</Dropdown.Header>
+          {services
+            .filter((service) => service.isRequired)
+            .map((service, i) => (
+              <Dropdown.Item
+                key={i}
+                description="obligatoire"
+                text={service.title}
+                as={Link}
+                to={`/services/${service.id}`}
+              />
+            ))}
+          <Divider />
+          <Dropdown.Header>{t('menu.subNav.optionnel')}</Dropdown.Header>
+          {services
+            .filter((service) => !service.isRequired)
+            .map((service, i) => (
+              <Dropdown.Item
+                key={i}
+                text={service.title}
+                as={Link}
+                to={`/services/${service.id}`}
+              />
+            ))}
+        </Dropdown.Menu>
+      </Dropdown>
+      <Menu.Item className="text">
+        <Link to="/bills">{t('menu.subNav.factures')}</Link>
+      </Menu.Item>
+    </Nav>
+  );
+};
 
 SubNavDesktopView.propTypes = {
   themes: PropTypes.arrayOf(PropTypes.instanceOf(Theme)).isRequired,
