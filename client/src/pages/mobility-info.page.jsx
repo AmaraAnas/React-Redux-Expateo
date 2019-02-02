8c40ebb37ccd1fd2798f97c3eb8af1bd44a0da18
AmaraAnas <anas94amara@gmail.com>
2019-02-02 16:45:08 +0100

    add handle submit function content with redirecting to start page

--- client/src/pages/mobility-info.page.jsx
+++ client/src/pages/mobility-info.page.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Container } from '../ui-kit';
+import { Redirect } from 'react-router-dom';
 import MobilityForm from '../pills/mobilities/mobility-form/mobilityForm.container';
 import styles from './base.module.css';
 
 export default class MobilityInfoForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { redirectToDashbord: false };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  //TODO: Add Handle Submit function content
-  handleSubmit() {}
+  handleSubmit() {
+    this.setState({
+      redirectToDashbord: true,
+    });
+  }
 
   render() {
+    let { redirectToDashbord } = this.state;
+    if (redirectToDashbord) {
+      return <Redirect from="/mobility-form" push={true} to="/" />;
+    }
     return (
       <Container className={styles.container} text fluid>
         <MobilityForm onSave={this.handleSubmit} />
